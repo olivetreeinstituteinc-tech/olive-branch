dfe3916a2835948c25a96bcbb1a20f040ca2dc41
Olive Tree Institute <olivetreeinstitute.inc@gmail.com>
2026-07-13 17:38:51 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "family-cards-v1";
+const ASSETS = ["./", "./index.html"];
+
+self.addEventListener("install", function (event) {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(
+        names
+          .filter(function (n) { return n !== CACHE_NAME; })
+          .map(function (n) { return caches.delete(n); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (resp) {
+      return resp || fetch(event.request);
+    })
+  );
+});
